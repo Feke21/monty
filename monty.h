519c36c6f0a66dcef90e1ab62769af65880c890e
Feke_21 <favouredubio@gmail.com>
2023-03-22 20:34:13 +0000

    Adding new files and header file

--- monty.h
+++ monty.h
@@ -1,6 +1,16 @@
 #ifndef MONTY_H
 #define MONTY_H
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -31,5 +41,43 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct arg_s
+{
+	int arg;
+	int flag;
+} arg_t;
+
+extern arg_t arg;
+
+/**
+ * struct glob_s - globally useful variables, all rolled into one
+ * @top: double pointer to top of stack
+ * @ops: double pointer to an instruction struct
+**/
+typedef struct glob_s
+{
+	stack_t **top;
+	instruction_t **ops;
+} glob_t;
+
+extern glob_t glob;
+
+
+/** get opcodes function */
+void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
+
+/** instruction 1 */
+void pall(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t **stack);
 
 #endif
