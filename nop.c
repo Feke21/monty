eecbf408e978d295a70b7c7f2f731ba8fa8d47bd
Emmanuel Mbumila <mbuxcompany@gmail.com>
2023-03-23 05:55:14 -0700

    add: function thad does nothing

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *nop_function- nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void nop_function(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
