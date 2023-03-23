e12b2189621a2a12d3abb2f4d60f32de3697a64c
Feke_21 <favouredubio@gmail.com>
2023-03-23 12:52:34 +0000

    Updating header file

--- monty.h
+++ monty.h
@@ -1,5 +1,5 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef MONTY
+#define MONTY
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,35 +12,7 @@
 #include <limits.h>
 #include <ctype.h>
 
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
-typedef struct stack_s
-{
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
-} stack_t;
-
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
+#define DELIMS "\n \t\r"
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -51,24 +23,45 @@ typedef struct instruction_s
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
-typedef struct arg_s
+typedef struct stack_s
 {
-	int arg;
-	int flag;
-} arg_t;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
 
-extern arg_t arg;
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
 
+/**
+ * struct glob_s - globally useful variables, all rolled into one
+ * @top: double pointer to top of stack
+ * @ops: double pointer to an instruction struct
+**/
+typedef struct glob_s
+{
+	stack_t **top;
+	instruction_t **ops;
+} glob_t;
 
-/** get opcodes function */
-void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
+extern glob_t glob;
 
-/** instruction 1 */
+/* get_opcodes.c */
+void get_opcodes(stack_t **stack, char *op, unsigned int line_number);
+
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
-void free_stack(stack_t **stack);
-
-/** swap code function */
-void swap_function(stack_t **head, unsigned int counter);
-
-#endif
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **head, unsigned int line_number);
