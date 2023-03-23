0403ca7d8af5167ad5caf06a5b6a5e363d5c3448
Feke_21 <favouredubio@gmail.com>
2023-03-23 15:25:25 +0000

    Update: header file

--- monty.h
+++ monty.h
@@ -65,20 +65,15 @@ void get_opcodes(stack_t **stack, char *op, unsigned int line_number);
 void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
 
 /** instruction 1 */
-void pall(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int counter);
+void _pall(stack_t **stack, unsigned int counter);
 void free_stack(stack_t **stack);
-
-/** swap code function */
-void swap_function(stack_t **head, unsigned int counter);
-
-/** add top two element function */
+void _swap(stack_t **head, unsigned int counter);
 void add_function(stack_t **head, unsigned int counter);
+void _nop(stack_t **head, unsigned int counter);
 
-/** function that do nothing */
-void nop_function(stack_t **head, unsigned int counter);
 
-/** substraction function */
-void  sub_function(stack_t **head, unsigned int counter);
+void  _sub(stack_t **head, unsigned int counter);
 
 /** functio that divide the second to element by the top one of the  stack */
 void div_function(stack_t **head, unsigned int counter);
