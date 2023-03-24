570b4514c8b7a96d268a38f6b26eb87fbc772c8d
Emmanuel Mbumila <mbuxcompany@gmail.com>
2023-03-23 18:26:42 -0700

    add: renaming function from nop_function to _nop

--- nop.c
+++ nop.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
-  *nop_function- nothing
+  *_nop- nothing
   *@head: stack head
   *@counter: line_number
   *Return: no return
  */
-void nop_function(stack_t **head, unsigned int counter)
+void _nop(stack_t **head, unsigned int counter)
 {
 	(void) counter;
 	(void) head;
